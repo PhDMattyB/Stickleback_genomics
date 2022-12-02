59cd1451f39b3668834ae268e16cbc6e3cf57c30
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2022-12-02 16:52:18 +0000

    Update Genomics_Functions.R

--- Genomics_Functions.R
+++ Genomics_Functions.R
@@ -66,3 +66,69 @@ stickle_CHR_reorder = function(data){
                                             'chr_Un'))
   
 }
+
+dist_cal = function(data){
+  data %>% 
+    group_by(CHR) %>% 
+    summarise(chr_len = max(POS)) %>% 
+    mutate(total = cumsum(chr_len)-chr_len) %>% 
+    dplyr::select(-chr_len) %>% 
+    left_join(data, ., by = c('CHR'='CHR')) %>%
+    arrange(CHR, 
+            POS) %>% 
+    mutate(BPcum = POS + total) 
+}
+
+
+axis_df = function(data){
+  dist_cal %>% 
+    group_by(CHR) %>% 
+    summarize(center=(max(BPcum) + min(BPcum))/2 )  
+}
+
+
+## not ready yet
+Fst_manhattan = function(non_outs, 
+                         outs, 
+                         axisdf,
+                         xval, 
+                         yval, 
+                         chr){
+  ggplot(non_outs, 
+         aes(x = {{xval}}, 
+             y = {{yval}}))+
+    # plot the non outliers in grey
+    geom_point(aes(color = as.factor(chr)), 
+               alpha = 0.8, 
+               size = 1.3)+
+    ## alternate colors per chromosome
+    scale_color_manual(values = rep(c("grey", "dimgrey"), 39))+
+    ## plot the outliers on top of everything
+    ## currently digging this hot pink colour
+    geom_point(data = outs,
+               col = '#264653',
+               alpha=0.8, 
+               size=1.3)+
+    scale_x_continuous(label = axisdf$CHR, 
+                       breaks = axisdf$center)+
+    scale_y_continuous(expand = c(0, 0), 
+                       limits = c(0,1.0))+
+    # geom_hline(yintercept = 0.00043, 
+    #            linetype = 2, 
+    #            col = 'Black')+
+    # ylim(0,1.0)+
+    # scale_y_reverse(expand = c(0, 0))+
+    # remove space between plot area and x axis
+    labs(x = 'Cumulative base pair', 
+         y = 'Fst', 
+         title = 'D)')+
+    theme(legend.position="none",
+          # panel.border = element_blank(),
+          panel.grid.major.x = element_blank(),
+          panel.grid.minor.x = element_blank(), 
+          axis.text.x = element_text(size = 9, 
+                                     angle = 90), 
+          axis.title = element_text(size = 14),
+          axis.title.x = element_blank(),
+          axis.text.y = element_text(size = 12))
+}
