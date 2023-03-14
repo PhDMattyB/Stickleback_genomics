8756bf9d471f439de1491e12d220d593eba59546
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2023-03-14 19:10:24 +0000

    Update Selective_sweep.R

--- Selective_sweep.R
+++ Selective_sweep.R
@@ -7,3 +7,26 @@
 ## 14.03.2023
 ##
 ##############################
+
+setwd('~/Parsons_Postdoc/Stickleback_Genomic/vcf_filter/')
+library(tidyverse)
+library(devtools)
+
+# zalpha package ----------------------------------------------------------
+
+# install_github("chorscroft/zalpha")
+
+library(zalpha)
+
+## Need a vector of snp locations
+stickle_map = read_tsv('stickleback_maf0.05_ldpruned_filtered.map', 
+                       col_names = c('CHR', 
+                                     'SNP', 
+                                     'GENO_POS', 
+                                     'POS'))
+snp_loc = stickle_map %>% 
+  select(POS)
+
+## This is really not working and R keep aborting
+# stickle_ped = read_table2('stickleback_maf0.05_ldpruned_filtered.ped') %>% 
+#   dplyr::select(1:13)
