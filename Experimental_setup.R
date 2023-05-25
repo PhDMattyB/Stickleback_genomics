f74a63fe54d3fa806e6de7c9b78915245d43e306
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2023-05-25 13:06:39 +0100

    Update Experimental_setup.R

--- Experimental_setup.R
+++ Experimental_setup.R
@@ -143,6 +143,7 @@ high_food_cold = treatment_data %>%
          Temperature == '12') %>% 
   group_by(Population, 
            Rep)%>% 
+  rename(Pop = Population) %>% 
   summarize(avg_weight = mean(Weight), 
             avg_length = mean(Length)) %>% 
   mutate(high_food_amount = avg_weight*0.3)
@@ -154,3 +155,58 @@ treatment_data %>%
            Rep) %>% 
   summarize(Number = n())
 
+
+
+# Cold side final ---------------------------------------------------------
+
+Final_data = read_csv('Experiment1_Sampling_Good.csv')
+
+high_food_cold_final = Final_data %>% 
+  # filter(Treatment == 'High Food', 
+  #        Population == 'ACAC') %>% 
+  filter(Treatment == 'High Food', 
+         Temp == '12') %>% 
+  group_by(Pop, 
+           Rep)%>% 
+  summarize(avg_weight_final = mean(Weight), 
+            avg_length_final = mean(Length))
+
+# low_food_cold_final = Final_data %>% 
+#   # filter(Treatment == 'Low Food', 
+#   #        Population == 'ACAC') %>% 
+#   filter(Treatment == 'Low Food', 
+#          Temp == '18') %>% 
+#   group_by(Pop, 
+#            Rep)%>% 
+#   summarize(avg_weight_final = mean(Weight), 
+#             avg_length_final = mean(Length))
+
+
+
+# Cold data compare --------------------------------------------------------
+
+high_cold_results = inner_join(high_food_cold, 
+                               high_food_cold_final, 
+                               by = c('Pop', 
+                                      'Rep')) %>% 
+  select(-'high_food_amount') %>% 
+  mutate(Weight_dif = avg_weight_final - avg_weight, 
+         Length_growth = avg_length_final - avg_length) %>% 
+  select(Pop, 
+         Rep, 
+         Weight_dif, 
+         Length_growth)
+
+# 
+# low_cold_results = inner_join(low_food_cold, 
+#                               low_food_cold_final, 
+#                               by = c('Pop', 
+#                                      'Rep')) %>% 
+#   select(-'low_food_amount') %>% 
+#   mutate(Weight_dif = avg_weight_final - avg_weight, 
+#          Length_growth = avg_length_final - avg_length) %>% 
+#   select(Pop, 
+#          Rep, 
+#          Weight_dif, 
+#          Length_growth)
+# 
