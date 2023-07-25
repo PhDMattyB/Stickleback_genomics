1ab7a15b0a61315bb06512b888505958d709b2ae
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2023-07-25 11:53:43 +0100

    Update Demographic_inference.R

--- Demographic_inference.R
+++ Demographic_inference.R
@@ -14,7 +14,7 @@ library(pegas)
 library(tidyverse)
 
 setwd('~/Parsons_Postdoc/Stickleback_Genomic/vcf_filter/')
-
+  s
 warm_stickle = read.vcf('stickleback_warm_pops.vcf', 
                         from = 1, 
                         to = 173444)
@@ -104,3 +104,14 @@ ggplot()+
                 y = Ne_median), 
             col = 'red')+
   scale_x_reverse()
+
+
+
+# nucleotide diversity ----------------------------------------------------
+
+warm_stickle = read.vcf('stickleback_warm_pops.vcf', 
+                        from = 1, 
+                        to = 173444)
+
+nuc.div(warm_stickle)
+
