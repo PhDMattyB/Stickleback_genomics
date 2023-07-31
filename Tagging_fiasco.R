cbf6fd38f278a5cadafc1fdb5944c5c857af8ea9
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2023-07-31 16:08:20 +0100

    Update Tagging_fiasco.R

--- Tagging_fiasco.R
+++ Tagging_fiasco.R
@@ -22,17 +22,27 @@ mean_vals = fiasco_data %>%
 
 ## The fucking 'problem' cross is bigger than the 'normals'
 
+## lets do some stats
+## Anova on body weight per cross
 weight_anova = aov(weight_gm ~ Cross, 
                    data = fiasco_data)
-
 summary(weight_anova)
+pairwise.t.test(fiasco_data$weight_gm,
+                fiasco_data$Cross, 
+                p.adjust.method = 'bonf')
+## Body weight does not differ per cross
 
+
+## Anova of length per cross
 length_anova = aov(length_mm ~ Cross, 
                    data = fiasco_data)
-
 summary(length_anova)
+pairwise.t.test(fiasco_data$length_mm,
+                fiasco_data$Cross, 
+                p.adjust.method = 'bonf')
+## length does not differ per cross
 
-
+## manova test to see if they differ in a multivariate framework
 dependent_vars = cbind(fiasco_data$weight_gm, 
                        fiasco_data$length_mm)
 
@@ -40,3 +50,5 @@ manova = manova(dependent_vars ~ Cross,
                 data = fiasco_data)
 
 summary(manova)
+## NOPE the crosses do NOT differ in a multivariate analysis of 
+## length and weight per cross
