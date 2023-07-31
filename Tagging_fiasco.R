6cd71d83d55caae9e759d1acd3567e963c615ea2
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2023-07-31 15:57:37 +0100

    Update Tagging_fiasco.R

--- Tagging_fiasco.R
+++ Tagging_fiasco.R
@@ -13,3 +13,9 @@ setwd('~/Parsons_Postdoc/')
 
 fiasco_data = read_csv('Scarred_fish_data.csv')
 
+mean_vals = fiasco_data %>% 
+  group_by(Cross) %>% 
+  mutate(mean_weight = mean(weight_gm), 
+         mean_length = mean(length_mm)) %>%
+  summarize(mean_weight = mean(weight_gm), 
+            mean_length = mean(length_mm)) 
