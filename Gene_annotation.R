f6d86f06f2b33695eb46a389ac01e7b4752f9265
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2023-08-08 16:32:43 +0100

    Update Gene_annotation.R

--- Gene_annotation.R
+++ Gene_annotation.R
@@ -13,17 +13,26 @@ setwd('~/Parsons_Postdoc/Stickleback_Genomic/Stickleback_Annotation_features/')
 library(tidyverse)
 
 
+read_tsv('genomic.gff', 
+         col_names = F, 
+         skip = 8) %>% 
+  select(X3) %>% 
+  distinct()
+
 geneid = read_tsv('genomic.gff', 
                   col_names = F, 
                   skip = 8) %>% 
-  arrange(X1, 
-          X4, 
+  filter(X3 %in% c('gene', 
+                   'exon', 
+                   'CDS')) %>% 
+  group_by(X1) %>% 
+  arrange(X4, 
           X5) %>% 
   ## arrange each gene by its start and end points on each chromosome
   mutate(mid = X4 + (X5-X4)/2) %>% 
   ## calculate the mid point of each gene from the start and end points
-  filter(mid >= 406599, 
-         mid <= 33118168) %>%
+  # filter(mid >= 406599, 
+  #        mid <= 33118168) %>%
   pull(X9) %>% 
   str_split_fixed(';', 
                   n = 5) %>% 
