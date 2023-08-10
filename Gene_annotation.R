a3d9fa60db80fe702e3f1e2e57b5ecc6955beb03
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2023-08-10 17:49:07 +0100

    Update Gene_annotation.R

--- Gene_annotation.R
+++ Gene_annotation.R
@@ -13,16 +13,10 @@ setwd('~/Parsons_Postdoc/Stickleback_Genomic/Stickleback_Annotation_features/')
 library(tidyverse)
 
 
-read_tsv('genomic.gff', 
-         col_names = F, 
-         skip = 8) %>% 
-  select(X3) %>% 
-  distinct()
-
 read_tsv('stickleback_v5_ensembl_genes.gff3.gz', 
          col_names = F, 
          skip = 8) %>% 
-  select(X2) %>% 
+  select(X3) %>% 
   distinct()
 
 genes_start = read_tsv('stickleback_v5_ensembl_genes.gff3.gz', 
@@ -123,4 +117,63 @@ genes_end_lineup = inner_join(methy_outliers,
 genes_end_lineup %>% 
   write_tsv('Methylation_data_genes_lineup.txt')
   
+
+# Alternative assembly information ----------------------------------------
+
+read_tsv('stickleback_v5_ensembl_genes.gff3.gz', 
+         col_names = F, 
+         skip = 8) %>% 
+  select(X3) %>% 
+  distinct()
+
+genes_start = read_tsv('genomic.gff', 
+                       col_names = F, 
+                       skip = 8) %>% 
+  # filter(X3 %in% c('gene', 
+  #                  'exon', 
+  #                  'CDS')) %>% 
+  group_by(X1) %>% 
+  arrange(X4, 
+          X5) %>% 
+  ## arrange each gene by its start and end points on each chromosome
+  mutate(mid = X4 + (X5-X4)/2) %>% 
+  dplyr::select(X1, 
+                X3:X5, 
+                X9:mid) %>% 
+  rename(chromosome = X1, 
+         feature = X3, 
+         start = X4, 
+         end = X5, 
+         gene_id = X9, 
+         position = mid) %>% 
+  dplyr::select(chromosome, 
+                feature, 
+                start, 
+                gene_id) %>% 
+  rename(position = start)
+
+methy_outliers = read_tsv('~/Parsons_Postdoc/Stickleback_Genomic/Methylation_outliers.txt')%>% 
+  separate(col = TETWarm, 
+           into = c('chromosome', 
+                    'position'),
+           sep = '-') %>% 
+  group_by(chromosome) %>% 
+  arrange(position)
+
+methy_outliers$position = as.numeric(methy_outliers$position)
+
+
+genes_start_lineup = inner_join(methy_outliers, 
+                                genes_start, 
+                                by = c('chromosome', 
+                                       'position')) %>% 
+  View()
+
+genes_end_lineup = inner_join(methy_outliers, 
+                              genes_end, 
+                              by = c('chromosome', 
+                                     'position')) 
+
+genes_end_lineup %>% 
+  write_tsv('Methylation_data_genes_lineup.txt')
 
