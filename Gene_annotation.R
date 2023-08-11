b81925b0c069f7d17e459626143afe4fecc1503c
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2023-08-11 17:00:21 +0100

    I'm a god

--- Gene_annotation.R
+++ Gene_annotation.R
@@ -19,81 +19,6 @@ read_tsv('stickleback_v5_ensembl_genes.gff3.gz',
   select(X3) %>% 
   distinct()
 
-genes_start = read_tsv('stickleback_v5_ensembl_genes.gff3.gz', 
-                  col_names = F, 
-                  skip = 8) %>% 
-  # filter(X3 %in% c('gene', 
-  #                  'exon', 
-  #                  'CDS')) %>% 
-  group_by(X1) %>% 
-  arrange(X4, 
-          X5) %>% 
-  ## arrange each gene by its start and end points on each chromosome
-  mutate(mid = X4 + (X5-X4)/2) %>% 
-  dplyr::select(X1, 
-                X3:X5, 
-                X9:mid) %>% 
-  rename(chromosome = X1, 
-         feature = X3, 
-         start = X4, 
-         end = X5, 
-         gene_id = X9, 
-         position = mid) %>% 
-  dplyr::select(chromosome, 
-                feature, 
-                start, 
-                gene_id) %>% 
-  rename(position = start)
-
-genes_end = read_tsv('stickleback_v5_ensembl_genes.gff3.gz', 
-                       col_names = F, 
-                       skip = 8) %>% 
-  # filter(X3 %in% c('gene', 
-  #                  'exon', 
-  #                  'CDS')) %>% 
-  group_by(X1) %>% 
-  arrange(X4, 
-          X5) %>% 
-  ## arrange each gene by its start and end points on each chromosome
-  mutate(mid = X4 + (X5-X4)/2) %>% 
-  dplyr::select(X1, 
-                X3:X5, 
-                X9:mid) %>% 
-  rename(chromosome = X1, 
-         feature = X3, 
-         start = X4, 
-         end = X5, 
-         gene_id = X9, 
-         position = mid) %>% 
-  dplyr::select(chromosome, 
-                feature, 
-                end, 
-                gene_id) %>% 
-  rename(position = end)
-
-# %>% 
-#   ## calculate the mid point of each gene from the start and end points
-#   # filter(mid >= 406599, 
-#   #        mid <= 33118168) %>%
-#   pull(X9) %>% 
-#   str_split_fixed(';', 
-#                   n = 5) %>% 
-#   as_tibble() %>%
-#   dplyr::select(V3) %>% 
-#   separate(col = V3, 
-#            into = c('geneid', 
-#                     'accenssionid'), 
-#            sep = ',') %>% 
-#   na.omit() %>% 
-#   dplyr::select(geneid) %>% 
-#   separate(col = geneid, 
-#            into = c('garbage', 
-#                     'geneid'), 
-#            sep = ':') %>% 
-#   dplyr::select(geneid)
-
-
-# Trying some new shit to line the genes up -------------------------------
 
 gene_annotation = read_tsv('stickleback_v5_ensembl_genes.gff3.gz', 
                      col_names = F, 
@@ -155,30 +80,76 @@ gene_overlap = foverlaps(gene_annotation,
 gene_overlap_tib = as_tibble(gene_overlap) %>% 
   na.omit() 
 
-gene_overlap_tib %>% 
-  pull(gene_id) %>% 
+
+gene_name_1 = gene_overlap_tib %>% 
+  # pull(gene_id) %>% 
   as_tibble() %>% 
-  separate(col = value, 
+  dplyr::select(chromosome, 
+                position, 
+                start, 
+                end, 
+                gene_id, 
+                F118, 
+                F218, 
+                TETWarm.F118, 
+                TETWarm.F218, 
+                TETWarm.F118.F218) %>% 
+  separate(col = gene_id, 
            into = c('ensemble_id', 
                     'gene_name', 
                     'parent_code', 
                     'gene_name2'), 
-           sep = ';')
-  # View()
+           sep = ';') %>%
+  separate(col = gene_name, 
+           into = c('Garbage', 
+                    'gene_name'), 
+           sep = '=') %>% 
+  dplyr::select(chromosome, 
+                position, 
+                start, 
+                end, 
+                gene_name) %>% 
+  na.omit()
+
+gene_name_2 = gene_overlap_tib %>% 
+  # pull(gene_id) %>% 
+  as_tibble() %>% 
+  dplyr::select(chromosome, 
+                position, 
+                start, 
+                end, 
+                gene_id,
+                F118, 
+                F218, 
+                TETWarm.F118, 
+                TETWarm.F218, 
+                TETWarm.F118.F218) %>% 
+  separate(col = gene_id, 
+           into = c('ensemble_id', 
+                    'gene_name', 
+                    'parent_code', 
+                    'gene_name2'), 
+           sep = ';') %>%
+  separate(col = parent_code, 
+           into = c('Garbage', 
+                    'gene_name'), 
+           sep = '=') %>% 
+  dplyr::select(chromosome, 
+                position, 
+                start, 
+                end, 
+                gene_name) %>% 
+  na.omit()
 
 
-
-# genes_start_lineup = inner_join(methy_outliers, 
-#            genes_start, 
-#            by = c('chromosome', 
-#                   'position')) %>% 
-#   View()
-# 
-# genes_end_lineup = inner_join(methy_outliers, 
-#            genes_end, 
-#            by = c('chromosome', 
-#                   'position')) 
-# 
-# genes_end_lineup %>% 
-#   write_tsv('Methylation_data_genes_lineup.txt')
-
+methy_genes = bind_rows(gene_name_1, 
+                        gene_name_2) %>% 
+  arrange(chromosome, 
+          position) %>% 
+  distinct(gene_name, 
+           .keep_all = T) %>% 
+  filter(!grepl('ENSG', 
+                gene_name))
+  
+methy_genes %>% 
+  write_csv('Methylation_outlier_genes.csv')
