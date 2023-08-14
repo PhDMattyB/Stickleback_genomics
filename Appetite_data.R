55318282d7e8e0022854675d1d4fb950c271dc92
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2023-08-14 12:34:02 +0100

    Update Appetite_data.R

--- Appetite_data.R
+++ Appetite_data.R
@@ -82,4 +82,58 @@ ggsave('Appetite_difference.tiff',
 
 
 
+# Time spent feeding ------------------------------------------------------
+
+Time_format = appetite_data %>% 
+  rename(overall_time = `overall time`)
+
+mean_time = Time_format %>% 
+  group_by(Location, 
+           Type) %>% 
+  summarise(mean_total_time = mean(overall_time))
+
+
+## stats to see if there is a difference in means
+## there isn't
+time_aov = aov(overall_time ~ pair*source_temp, 
+               data = Time_format)
+
+summary(time_aov)
+
+
+
+appetite_cols = c('#003049', 
+                  '#c1121f')
+
+eating_time_plot = ggplot(data = Time_format, 
+                       aes(x = Location, 
+                           y = overall_time, 
+                           fill = Type), 
+                       col = 'black') +
+  # geom_boxplot()+
+  geom_violin() +
+  stat_summary(fun = "mean", 
+               geom = "point", 
+               position = position_dodge(0.9), 
+               size = 3)+
+  # geom_point(data = appetite_means, 
+  #            aes(x = Location, 
+  #                y = mean_pellets), 
+  #            col = 'black', 
+  #            size = 3)+
+  scale_fill_manual(values = appetite_cols)+
+  labs(y = 'Pellets eaten to satiation')+
+  theme(panel.grid = element_blank(), 
+        axis.title.x = element_blank(), 
+        axis.title.y = element_text(size = 14), 
+        axis.text = element_text(size = 12))
+
+eating_time_plot
+
+ggsave('Time_spent_eating_difference.tiff', 
+       plot = eating_time_plot, 
+       dpi = 'retina', 
+       units = 'cm', 
+       height = 10, 
+       width = 15)
 
