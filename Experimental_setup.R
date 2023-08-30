3184fc5022508ea32e6df86a9116eb79219b8202
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2023-08-30 16:51:48 +0100

    Update Experimental_setup.R

--- Experimental_setup.R
+++ Experimental_setup.R
@@ -152,7 +152,7 @@ inner_join(low_food_warm,
          Weight_dif, 
          Length_growth, 
          cond_fac_diff) %>% 
-  ungroup() %>% 
+  # ungroup() %>% 
   summarize(mean_weight_dif = mean(Weight_dif), 
             mean_length_dif = mean(Length_growth), 
             mean_cond_dif = mean(cond_fac_diff))
@@ -217,14 +217,15 @@ low_food_cold = treatment_data %>%
          Temp = Temperature) %>% 
   summarize(avg_weight = mean(Weight), 
             avg_length = mean(Length)) %>% 
-  mutate(low_food_amount = avg_weight*0.02)
+  mutate(low_food_amount = avg_weight*0.02, 
+         low_cond_fac = avg_weight/avg_length^(1/3)*100)
 
-treatment_data %>% 
-  filter(Treatment == 'Low Food', 
-         Temperature == '12') %>% 
-  group_by(Population, 
-           Rep) %>% 
-  summarize(Number = n())
+# treatment_data %>% 
+#   filter(Treatment == 'Low Food', 
+#          Temperature == '12') %>% 
+#   group_by(Population, 
+#            Rep) %>% 
+#   summarize(Number = n())
 
 
 high_food_cold = treatment_data %>% 
@@ -239,14 +240,15 @@ high_food_cold = treatment_data %>%
          Temp = Temperature) %>% 
   summarize(avg_weight = mean(Weight), 
             avg_length = mean(Length)) %>% 
-  mutate(high_food_amount = avg_weight*0.3)
+  mutate(high_food_amount = avg_weight*0.3, 
+         low_cond_fac = avg_weight/avg_length^(1/3)*100)
 
-treatment_data %>% 
-  filter(Treatment == 'High Food', 
-         Temperature == '12') %>% 
-  group_by(Population, 
-           Rep) %>% 
-  summarize(Number = n())
+# treatment_data %>% 
+#   filter(Treatment == 'High Food', 
+#          Temperature == '12') %>% 
+#   group_by(Population, 
+#            Rep) %>% 
+#   summarize(Number = n())
 
 
 
@@ -263,7 +265,8 @@ high_food_cold_final = Final_data %>%
            Rep, 
            Temp)%>% 
   summarize(avg_weight_final = mean(Weight), 
-            avg_length_final = mean(Length))
+            avg_length_final = mean(Length),
+            cond_fac_final = avg_weight_final/avg_length_final^(1/3)*100)
 
 low_food_cold_final = Final_data %>%
   # filter(Treatment == 'Low Food',
@@ -274,7 +277,8 @@ low_food_cold_final = Final_data %>%
            Rep, 
            Temp)%>%
   summarize(avg_weight_final = mean(Weight),
-            avg_length_final = mean(Length))
+            avg_length_final = mean(Length),
+            cond_fac_final = avg_weight_final/avg_length_final^(1/3)*100)
 
 
 
@@ -308,6 +312,74 @@ low_cold_results = inner_join(low_food_cold,
          Temp,
          Weight_dif,
          Length_growth)
+
+
+
+# cold expectations -------------------------------------------------------
+inner_join(low_food_cold, 
+           low_food_cold_final, 
+           by = c('Pop', 
+                  'Rep', 
+                  'Temp')) %>% 
+  select(-'low_food_amount') %>% 
+  mutate(Weight_dif = avg_weight_final - avg_weight, 
+         Length_growth = avg_length_final - avg_length, 
+         cond_fac_diff = cond_fac_final - low_cond_fac)%>% 
+  select(Pop, 
+         Rep, 
+         Temp,
+         Weight_dif, 
+         Length_growth, 
+         cond_fac_diff) %>% 
+  # ungroup() %>% 
+  summarize(mean_weight_dif = mean(Weight_dif), 
+            mean_length_dif = mean(Length_growth), 
+            mean_cond_dif = mean(cond_fac_diff))
+
+
+cold_morph_weight_loss_cold = inner_join(low_food_cold, 
+                                         low_food_cold_final, 
+                                         by = c('Pop', 
+                                                'Rep', 
+                                                'Temp')) %>% 
+  select(-'low_food_amount') %>% 
+  mutate(Weight_dif = avg_weight_final - avg_weight, 
+         Length_growth = avg_length_final - avg_length, 
+         cond_fac_diff = cond_fac_final - low_cond_fac)%>% 
+  select(Pop, 
+         Rep, 
+         Temp,
+         Weight_dif, 
+         Length_growth, 
+         cond_fac_diff) %>% 
+  ungroup() %>% 
+  filter(Pop %in% c('ACAC', 
+                    'MCMC')) %>% 
+  summarize(mean_weight_dif = mean(Weight_dif), 
+            mean_length_dif = mean(Length_growth), 
+            mean_cond_dif = mean(cond_fac_diff))
+
+warm_morph_weight_loss_cold = inner_join(low_food_cold, 
+                                         low_food_cold_final, 
+                                         by = c('Pop', 
+                                                'Rep', 
+                                                'Temp')) %>% 
+  select(-'low_food_amount') %>% 
+  mutate(Weight_dif = avg_weight_final - avg_weight, 
+         Length_growth = avg_length_final - avg_length, 
+         cond_fac_diff = cond_fac_final - low_cond_fac)%>% 
+  select(Pop, 
+         Rep, 
+         Temp,
+         Weight_dif, 
+         Length_growth, 
+         cond_fac_diff) %>% 
+  ungroup() %>% 
+  filter(Pop %in% c('AWAW', 
+                    'MWMW')) %>% 
+  summarize(mean_weight_dif = mean(Weight_dif), 
+            mean_length_dif = mean(Length_growth), 
+            mean_cond_dif = mean(cond_fac_diff))
 
 
 # Cold warm comparison ----------------------------------------------------
