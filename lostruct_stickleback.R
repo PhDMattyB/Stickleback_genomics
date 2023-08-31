2a9f2d534c1bc7203d30a27265ad5a97e7eb16cc
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2023-08-31 12:38:03 +0100

    Update lostruct_stickleback.R

--- lostruct_stickleback.R
+++ lostruct_stickleback.R
@@ -12,4 +12,21 @@ setwd('~/Parsons_Postdoc/Stickleback_Genomic/vcf_filter/')
 library(tidyverse)
 library(data.table)
 
-ped_data = read_table2('stickleback_maf0.05_ldpruned_filtered.ped')
+ped_data = read_table('stickleback_maf0.05_ldpruned_filtered.ped',
+                      col_names = F) %>% 
+  rename(IndividualID = X2, 
+         Population = X1, 
+         MaternalID = X3, 
+         PaternalID = X4, 
+         Phenotype = X6, 
+         Sex = X5)
+
+
+map_data = read_tsv('stickleback_maf0.05_ldpruned_filtered.map', 
+                    col_names = c('CHR', 
+                                  'SNP', 
+                                  'GPOS', 
+                                  'POS'))
+
+Create_tped(ped = ped_data, 
+            map = map_data)
