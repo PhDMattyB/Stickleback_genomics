c67726aacbdb5b14336b8905a37fc3cae7a32fc2
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2023-10-12 09:30:06 +0100

    Update rehh_selective_sweep.R

--- rehh_selective_sweep.R
+++ rehh_selective_sweep.R
@@ -31,8 +31,7 @@ myvw = data2haplohh(hap_file = 'MYVW_genotypes.vcf',
                     allele_coding = 'map')
 
 
-# iHS haplotype scan ------------------------------------------------------
-
+# iHS haplotype scan
 
 myvc_scan = scan_hh(myvc, polarized = F)
 myvw_scan = scan_hh(myvw, polarized = F)
@@ -67,8 +66,7 @@ ggplot(myvc_ihs$ihs,
 
 
 
-# xp-ehh analysis (cross population) --------------------------------------
-
+# xp-ehh analysis (cross population) 
 myvw_myvc = ies2xpehh(myvw_scan, 
                       myvc_scan, 
                       popname1 = 'MYVW', 
@@ -88,8 +86,7 @@ ggplot(myvw_myvc,
 
 
 
-# Haplotype structure around selection target -----------------------------
-# find the highest hit
+# Haplotype structure around selection target 
 hit = myvw_myvc %>% 
   arrange(desc(LOGPVALUE)) %>% 
   top_n(1)
@@ -119,6 +116,205 @@ plot(myvw_haplen)
 plot(myvc_haplen)
 
 
+# ASHN signatures of selection ------------------------------------------
+
+
+ashnc = data2haplohh(hap_file = 'ashnC_genotypes.vcf', 
+                    polarize_vcf = F, 
+                    min_maf = 0.05, 
+                    chr.name = 'chr_XXI', 
+                    allele_coding = 'map')
+
+ashnw = data2haplohh(hap_file = 'ashnW_genotypes.vcf', 
+                    polarize_vcf = F, 
+                    min_maf = 0.05, 
+                    chr.name = 'chr_XXI', 
+                    allele_coding = 'map')
+
+
+# iHS haplotype scan
+
+ashnc_scan = scan_hh(ashnc, polarized = F)
+ashnw_scan = scan_hh(ashnw, polarized = F)
+
+## perform ihs selective scan
+ashnw_ihs = ihh2ihs(ashnw_scan, 
+                   freqbin = 1)
+ashnc_ihs = ihh2ihs(ashnc_scan, 
+                   freqbin = 1)
+
+## plot the ihs statistic
+ggplot(ashnw_ihs$ihs, 
+       aes(POSITION, 
+           IHS))+
+  geom_point()
+
+ggplot(ashnc_ihs$ihs, 
+       aes(POSITION, 
+           IHS))+
+  geom_point()
+
+## plot the pvalues
+ggplot(ashnw_ihs$ihs, 
+       aes(POSITION, 
+           LOGPVALUE))+
+  geom_point()
+
+ggplot(ashnc_ihs$ihs, 
+       aes(POSITION, 
+           LOGPVALUE))+
+  geom_point()
+
+
+
+# xp-ehh analysis (cross population) 
+ashnw_ashnc = ies2xpehh(ashnw_scan, 
+                      ashnc_scan, 
+                      popname1 = 'ashnW', 
+                      popname2 = 'ashnC', 
+                      include_freq = T)
+
+# plot
+
+ggplot(ashnw_ashnc, 
+       aes(POSITION, 
+           XPEHH_ashnW_ashnC))+
+  geom_point()
+
+ggplot(ashnw_ashnc, 
+       aes(POSITION, 
+           LOGPVALUE)) + geom_point()
+
+
+
+# Haplotype structure around selection target 
+# hit = ashnw_ashnc %>% 
+#   arrange(desc(LOGPVALUE)) %>% 
+#   top_n(1)
+
+# get SNP position
+# x = hit$POSITION
+# 
+# marker_id_warm = which(ashnw@positions == x)
+# marker_id_cold = which(ashnc@positions == x)
+# 
+# ashnw_furcation = calc_furcation(ashnw, 
+#                                 mrk = marker_id_warm)
+# 
+# ashnc_furcation = calc_furcation(ashnc, 
+#                                 mrk = marker_id_cold )
+# 
+# 
+# plot(ashnw_furcation, 
+#      xlim = c(58695, 17420697))
+# plot(ashnc_furcation, 
+#      xlim = c(58695, 17420697))
+# 
+# ashnw_haplen = calc_haplen(ashnw_furcation)
+# ashnc_haplen = calc_haplen(ashnc_furcation)
+# 
+# plot(ashnw_haplen)
+# plot(ashnc_haplen)
+
+# SKR signatures of selection ------------------------------------------
+
+
+skrc = data2haplohh(hap_file = 'skrC_genotypes.vcf', 
+                    polarize_vcf = F, 
+                    min_maf = 0.05, 
+                    chr.name = 'chr_XXI', 
+                    allele_coding = 'map')
+
+skrw = data2haplohh(hap_file = 'skrW_genotypes.vcf', 
+                    polarize_vcf = F, 
+                    min_maf = 0.05, 
+                    chr.name = 'chr_XXI', 
+                    allele_coding = 'map')
+
+
+# iHS haplotype scan
+
+skrc_scan = scan_hh(skrc, polarized = F)
+skrw_scan = scan_hh(skrw, polarized = F)
+
+## perform ihs selective scan
+skrw_ihs = ihh2ihs(skrw_scan, 
+                   freqbin = 1)
+skrc_ihs = ihh2ihs(skrc_scan, 
+                   freqbin = 1)
+
+## plot the ihs statistic
+ggplot(skrw_ihs$ihs, 
+       aes(POSITION, 
+           IHS))+
+  geom_point()
+
+ggplot(skrc_ihs$ihs, 
+       aes(POSITION, 
+           IHS))+
+  geom_point()
+
+## plot the pvalues
+ggplot(skrw_ihs$ihs, 
+       aes(POSITION, 
+           LOGPVALUE))+
+  geom_point()
+
+ggplot(skrc_ihs$ihs, 
+       aes(POSITION, 
+           LOGPVALUE))+
+  geom_point()
+
+
+
+# xp-ehh analysis (cross population) 
+skrw_skrc = ies2xpehh(skrw_scan, 
+                      skrc_scan, 
+                      popname1 = 'skrW', 
+                      popname2 = 'skrC', 
+                      include_freq = T)
+
+# plot
+
+ggplot(skrw_skrc, 
+       aes(POSITION, 
+           XPEHH_skrW_skrC))+
+  geom_point()
+
+ggplot(skrw_skrc, 
+       aes(POSITION, 
+           LOGPVALUE)) + geom_point()
+
+
+
+# Haplotype structure around selection target 
+# hit = skrw_skrc %>% 
+#   arrange(desc(LOGPVALUE)) %>% 
+#   top_n(1)
+# 
+# # get SNP position
+# x = hit$POSITION
+# 
+# marker_id_warm = which(skrw@positions == x)
+# marker_id_cold = which(skrc@positions == x)
+# 
+# skrw_furcation = calc_furcation(skrw, 
+#                                 mrk = marker_id_warm)
+# 
+# skrc_furcation = calc_furcation(skrc, 
+#                                 mrk = marker_id_cold )
+# 
+# 
+# plot(skrw_furcation, 
+#      xlim = c(58695, 17420697))
+# plot(skrc_furcation, 
+#      xlim = c(58695, 17420697))
+# 
+# skrw_haplen = calc_haplen(skrw_furcation)
+# skrc_haplen = calc_haplen(skrc_furcation)
+# 
+# plot(skrw_haplen)
+# plot(skrc_haplen)
 
 
 # Warm vs Cold sweep detect (chr 21) --------------------------------------
@@ -224,7 +420,7 @@ plot(warm_haplen)
 plot(cold_haplen)
 
 
-# Warm vs Cold sweep detect (chr 21) --------------------------------------
+# Warm vs Cold sweep detect (chr 21) NO GTS or CSWY --------------------------------------
 
 warm2 = data2haplohh(hap_file = 'Warm_morphs_NOGTS_genotypes.vcf', 
                     polarize_vcf = F, 
