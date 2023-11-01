669d12cd3a01e47e4b5425580c817cc3aa2ff12b
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2023-11-01 15:56:49 +0000

    Create AQ9_lookup.R

---
+++ AQ9_lookup.R
@@ -0,0 +1,17 @@
+##############################
+## AQ9 stickleback look up
+##
+## Matt Brachmann (PhDMattyB)
+##
+## 01.11.2023
+##
+##############################
+
+
+library(tidyverse)
+
+setwd('~/Parsons_Postdoc/')
+
+df = read_csv('AQ9_Tank_map_R.csv')
+
+
