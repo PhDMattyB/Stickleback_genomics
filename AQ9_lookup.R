cc613bad90b6fda1097cf244cb6b067efb46542f
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2023-11-01 16:08:28 +0000

    Update AQ9_lookup.R

--- AQ9_lookup.R
+++ AQ9_lookup.R
@@ -15,16 +15,36 @@ setwd('~/Parsons_Postdoc/')
 df = read_csv('AQ9_Tank_map_R.csv')
 
 
-tank_lookup = function(df, Side = 'all', Tank_num = "all"){
-  df %>% 
-    filter(Side == 'Cold') %>% 
-    filter(Tank_num == '249')
+tank_lookup = function(filename, Side = 'all', Tank_num = "all"){
+  df = read_csv(filename) %>% 
+    filter(Side == Side) %>% 
+    filter(Tank_num == Tank_num)
+  
+  if(Side != 'all'){
+    sub_df = df %>%
+      filter(Side == Side)
+    return(sub_df)
+  }
+  if(Tank_num != 'all'){
+    sub_df2 = sub_df %>%
+      filter(Tank_num == Tank_num)
+    return(sub_df2)
+  }
+  
+return(df)
+  
+  
 }
+
+
+tank_lookup(filename = 'AQ9_Tank_map_R.csv', 
+            Side = 'Cold', 
+            Tank_num = '249')
+
 
 df %>% 
   filter(Side == 'Cold') %>% 
   filter(Tank_num == '249')
-
 
 df %>% 
   filter(Side == 'Cold') %>% 
