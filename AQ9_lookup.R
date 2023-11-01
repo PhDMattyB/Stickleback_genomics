48e2a76c1458dd3376de089cda01099397778549
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2023-11-01 16:38:17 +0000

    Update AQ9_lookup.R

--- AQ9_lookup.R
+++ AQ9_lookup.R
@@ -14,41 +14,28 @@ setwd('~/Parsons_Postdoc/')
 
 df = read_csv('AQ9_Tank_map_R.csv')
 
-df %>% 
-  filter(Side == 'Cold') %>% 
-  filter(Tank_num == '249')
-
-
-tank_lookup = function(filename, Side = '', Tank_num = ''){
-  df = read_csv(filename) 
+tank_lookup = function(data, Side, Side_room, Tank_num, num){
   
-  df = df %>% 
-    filter(Side == '') %>% 
-    filter(Tank_num == '')
-  # 
-  # if(Side != 'all'){
-  #   sub_df = df %>%
-  #     filter(Side == Side)
-  #   return(sub_df)
-  # }
-  # if(Tank_num != 'all'){
-  #   sub_df2 = sub_df %>%
-  #     filter(Tank_num == Tank_num)
-  #   return(sub_df2)
-  # }
+  # Side <- enquo(Side)
+  # Tank_num <- enquo(Tank_num)
+  Side = rlang::parse_expr(quo_name(enquo(Side)))
+  Tank_num = rlang::parse_expr(quo_name(enquo(Tank_num)))
   
-return(df)
-  
+  data %>% 
+    filter(Side == Side_room, 
+           Tank_num == num) 
   
 }
 
 
-tank_lookup(filename = 'AQ9_Tank_map_R.csv', 
-            Side = 'Cold', 
-            Tank_num = '249')
+tank_lookup(data = df, 
+            Side,
+            Side_room = 'Cold', 
+            Tank_room,
+            num = '250')
 
-
-
-df %>% 
-  filter(Side == 'Cold') %>% 
-  filter(Cross_num == '203')
+# 
+# 
+# df %>% 
+#   filter(Side == 'Cold') %>% 
+#   filter(Cross_num == '203')
