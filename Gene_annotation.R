107bfe1ae64fc3de17c4973399cb39da2e3fabc7
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2024-03-19 16:20:20 +0000

    Update Gene_annotation.R

--- Gene_annotation.R
+++ Gene_annotation.R
@@ -43,19 +43,25 @@ gene_annotation = read_tsv('stickleback_v5_ensembl_genes.gff3.gz',
 
 genes = gene_annotation %>% 
   arrange(chromosome) %>% 
-  filter(feature == 'gene') %>% 
-  select(chromosome, 
-         feature, 
-         gene_id, 
-         position)
+  filter(feature == 'gene') 
+# %>% 
+#   select(chromosome, 
+#          feature, 
+#          gene_id, 
+#          position)
 
-WC_Fst_regions = read_csv('WC_25Kb_0.5%_Fst_outlier.csv') %>% 
-  rename(start = win_start, 
-         end = win_end, 
-         position = win_mid, 
-         chromosome = CHR) %>% 
+ASHN_div_snps = read_csv('ASHN_TOP_DAWG_Fst_clean.csv') %>% 
+  select(-NMISS, 
+         -FST) %>%
+  rename(position = POS, 
+         chromosome = CHR, 
+         FST = FST_zero) %>% 
+  filter(value == 'Outlier') %>% 
   group_by(chromosome)
 
+ASHN_div_snps %>% 
+  summarise(snp_per_chr = n()) %>% 
+  View()
 
 ## Cannot figure this out right now
 ## Need to find out if the average positions of each gene
@@ -75,6 +81,81 @@ gene_test = genes %>%
 
 test_data = WC_Fst_regions %>% 
   filter(chromosome == 'chr_I')
+
+
+# Test old R functions  ---------------------------------------------------
+
+library(tidyverse)
+#its the only package from the original list we actually need >:(
+
+#alter this line to the directory on your computer containing the files to be analyzed
+
+# this function takes a .gff or .gff3 file and creates a tibble
+# arguments:
+# filename -  the name of the gff file
+# lead_skip - this argument specifies the
+# chromosome - the chromosome to subset from the full gff, default is 'all' and no subset is performed.
+read_gff = function(filename, lead_skip = 9, chromosome = "all"){
+  #CAM - personally I would pass a vector of names to the read_tsv as opposed to renaming in the pipe (this could be an argument too).
+  colnames = c("chr", "source", "feature", "start", "end", "score", "strand", "frame", "attribute" )
+  #read in the raw file with readr
+  gff = read_tsv(filename, col_names = colnames, skip = lead_skip) %>%
+    filter(feature == 'gene') %>% 
+    arrange(start, end) %>% 
+    mutate(mid = start + (end-start)/2) ## arrange each gene by its start and end points on each chromosome
+  #subset if user changed the chromosome argument
+  if(chromosome != "all"){
+    sub_gff = gff %>% filter(chr == chromosome)
+    return(sub_gff)
+  }
+  #if the default chr arg was not changed, return full gff
+  return(gff)
+}
+
+# this function takes a tab delimited file of outlier loci and creates a tibble
+# arguments:
+# filename -  the name of the gff file
+# chromosome - the chromosome to subset from the full gff, default is 'all' and no subset is performed.
+read_outlier = function(filename, chromosome = "all"){
+  #read in the dataframe
+  out_dat = read_csv(filename) %>% 
+    arrange(chromosome) 
+  # %>% 
+  #   filter(chromo != 'Contigs')
+  # #subset if user changed the chromosome argument
+  if(chromosome != "all"){
+    sub_out = out_dat %>% 
+      filter(chromosome == chromosome) 
+    return(sub_out)
+  }
+  #if the default chr arg was not changed, return full gff
+  return(out_dat)
+}
+
+
+
+#I set important filenames etc. to variables, so they can be easily found, changed, and reused.
+gff_filename = "stickleback_v5_ensembl_genes.gff3.gz"
+ncbi_code_AC08 = "chr_I"
+outlier_data = "ASHN_TOP_DAWG_Fst_clean.csv"
+outlier_code_AC08 = "chr_I"
+
+########
+# read in the data
+# all the detail above is abstracted away into these two lines
+gff = read_gff(gff_filename, chromosome = 'all')
+outliers = read_outlier(outlier_data, chromosome = 'all')
+
+# obtain outlier positions
+pos = outliers$POS
+
+#obtain genes in those positions
+gene_regions = gff %>% 
+  mutate(hit_dist = abs(mid - pos)) %>% 
+  arrange(hit_dist) %>% 
+  filter(hit_dist <100) %>%
+  select(chr, start, end, attribute, hit_dist) %>% 
+  pull(attribute)
 
 
 # Methylation outliers ----------------------------------------------------
